5224c5e366236b86853d1a06a59c77bdc619cde1
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-08-09 23:35:23 -0300

    added firebase to project

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,17 @@
 import { User } from "./access/user.model";
+import * as firebase from 'firebase';
 
 export class AuthService {
     public userRegister(user: User): void {
-        console.log('serviço de autenticação: ', user);
+        //console.log('serviço de autenticação: ', user);
+
+        firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+            .then((response: any) => {
+                // console.log(response);
+                delete user.password;
+                firebase.database().ref(`userDetail/${btoa(user.email)}`)
+                    .set(user);
+            })
+            .catch((error: Error) => console.log(error));
     }
 }

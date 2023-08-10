fc5789d481c4bc87018ede32c9582be46f082ad1
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-08-09 23:47:41 -0300

    Added reactive form to login template

--- src/app/access/login/login.component.ts
+++ src/app/access/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +11,23 @@ export class LoginComponent implements OnInit {
 
   @Output() public showPanel: EventEmitter<string> = new EventEmitter<string>();
 
+  public form: FormGroup = new FormGroup({
+    'email': new FormControl(null),
+    'password': new FormControl(null)
+  })
+
   constructor() { }
 
   ngOnInit() {
   }
 
-
   public showRegisterPanel():void {
     console.log('estou aqui');
     this.showPanel.emit('register');
+  }
 
+  public authenticate(): void {
+    console.log(this.form);
   }
 
 }
